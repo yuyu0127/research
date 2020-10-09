09b06d1a13f5be62553a7a9d0735fe5427c25fb5
yuyu0127 <saiyuki01271210@gmail.com>
2020-10-09 19:53:59 +0900

    座標アンカー追加

--- Assets/Scripts/LightController.cs
+++ Assets/Scripts/LightController.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 public class LightController : MonoBehaviour
 {
     [SerializeField] OVRInput.Controller controllerType;
-    [SerializeField] Vector3 positionScale;
-    [SerializeField] Vector3 positionBase;
     [SerializeField] bool autoMove;
 
     Light light;
@@ -15,6 +13,9 @@ public class LightController : MonoBehaviour
     float intervalPos = 0f;
     float timerRot = 0f;
     float intervalRot = 0f;
+
+    Vector3 anchor1;
+    Vector3 anchor2;
 
     void Start()
     {
@@ -29,8 +30,14 @@ public class LightController : MonoBehaviour
         }
         if (!autoMove)
         {
-            var pos = OVRInput.GetLocalControllerPosition(controllerType);
-            transform.position = Vector3.Scale(pos - positionBase, positionScale);
+            Vector3 controllerPos = OVRInput.GetLocalControllerPosition(controllerType);
+            var tx = (controllerPos.x - anchor1.x) / (anchor2.x - anchor1.x);
+            var ty = (controllerPos.y - anchor1.y) / (anchor2.y - anchor1.y);
+            var tz = (controllerPos.z - anchor1.z) / (anchor2.z - anchor1.z);
+            var posX = Mathf.LerpUnclamped(-15f, 15, tx);
+            var posY = Mathf.LerpUnclamped(-8.4375f, 8.4375f, ty);
+            var posZ = Mathf.LerpUnclamped(-1f, 0f, tz);
+            transform.position = new Vector3(posX, posY, posZ);
             transform.rotation = OVRInput.GetLocalControllerRotation(controllerType);
         }
         else
